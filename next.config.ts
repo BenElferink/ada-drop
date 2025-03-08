bc869de2fab8205ed94a40718fe38e7763f795ae
Ben Elferink <ben.elferink@icloud.com>
2025-03-08 21:33:37 +0200

    fix: kill headers config

--- next.config.ts
+++ next.config.ts
@@ -15,36 +15,6 @@ const nextConfig: NextConfig = {
     }
     return config
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; script-src 'self' 'unsafe-eval'; connect-src 'self'; frame-src 'self'; worker-src 'self';",
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 export default nextConfig
