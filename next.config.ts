0cc64ad70ea38e12554534542a07e31b1aa117a7
Ben Elferink <ben.elferink@icloud.com>
2025-03-08 21:27:21 +0200

    fix: next api cors

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,30 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     styledComponents: true,
   },
-  images: {
-    unoptimized: true,
+  webpack: function (config) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    }
+    return config
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'OPTIONS,GET,POST' },
+        ],
+      },
+    ]
   },
 }
 
