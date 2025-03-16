ca0f5bca2b3284f2ed2a8db0254ae67ffa1e49a2
Ben Elferink <ben.elferink@icloud.com>
2025-03-16 22:09:52 +0200

    feat: mobile header status

--- src/components/header-mobile.tsx
+++ src/components/header-mobile.tsx
@@ -3,7 +3,8 @@ import { TITLE } from '@/constants'
 import { CardanoLogo } from '@/icons'
 import styled from 'styled-components'
 import { ToggleDarkMode } from '@odigos/ui-kit/containers'
-import { Header as Container, Text } from '@odigos/ui-kit/components'
+import { Header as Container, Status, Text } from '@odigos/ui-kit/components'
+import { NOTIFICATION_TYPE } from '@odigos/ui-kit/types'
 
 const OverrideStyles = styled.div`
   width: 100%;
@@ -22,12 +23,15 @@ export const HeaderMobile = () => {
     <OverrideStyles>
       <Container
         left={[
-          <CardanoLogo key='logo' size={50} />,
+          <CardanoLogo key='logo' size={40} />,
           <Text key='title' family='secondary' size={20}>
             {TITLE}
           </Text>,
         ]}
-        right={[<ToggleDarkMode key='toggle-theme' />]}
+        right={[
+          <Status key='use-desktop' status={NOTIFICATION_TYPE.WARNING} title='Please use Desktop' withBorder withBackground />,
+          <ToggleDarkMode key='toggle-theme' />,
+        ]}
       />
     </OverrideStyles>
   )
