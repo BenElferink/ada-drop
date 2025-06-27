5e2680f34f0ebbcdb92be8a53a7777a4cfe44077
Ben Elferink <ben.elferink@icloud.com>
2025-06-27 12:49:25 +0300

    fix: enhance transaction confirmation logic with retries and delays
    - Added a 30-second wait after a successful transaction confirmation to ensure subsequent transactions can be processed.
    - Implemented retry logic with a 1-second delay for cases where the transaction data is not found or an error occurs.

--- src/functions/txConfirmation.ts
+++ src/functions/txConfirmation.ts
@@ -7,8 +7,12 @@ export const txConfirmation = async (_txHash: string): Promise<Transaction> => {
     const data = await api.transaction.getData(_txHash)
 
     if (data.block) {
+      // success, let's wait a bit more to make sure the next tx in the batch is able to be processed
+      await sleep(30000) // 30 seconds
       return data
     } else {
+      // retry...
+      // in theory this should never happen, only in catch block, but just in case
       await sleep(1000)
       return await txConfirmation(_txHash)
     }
@@ -16,6 +20,7 @@ export const txConfirmation = async (_txHash: string): Promise<Transaction> => {
     const errMsg = error?.response?.data || error?.message || error?.toString() || 'UNKNOWN ERROR'
 
     if (errMsg === `The requested component has not been found. ${_txHash}`) {
+      // retry...
       await sleep(1000)
       return await txConfirmation(_txHash)
     } else {
