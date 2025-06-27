1b3293a1b9d9edb0eab1f31ff6b1fb5742560074
Ben Elferink <ben.elferink@icloud.com>
2025-06-27 13:54:29 +0300

    feat: add social media icons to header component
    - Introduced a new SocialIcon component to display social media links.
    - Updated the Header component to include icons for Twitter, Discord, GitHub, and X, enhancing user engagement options.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { TITLE } from '@/constants'
 import { CardanoLogo } from '@/icons'
+import { SocialIcon } from './social-icon'
 import { MeshProvider } from '@meshsdk/react'
 import { StatusType } from '@odigos/ui-kit/types'
+import { useDarkMode } from '@odigos/ui-kit/store'
 import { useChainLoad, useEpochInfo } from '@/hooks'
 import { ConnectWallet, NewAirdrop } from '@/containers'
 import { ToggleDarkMode } from '@odigos/ui-kit/containers'
@@ -11,6 +13,7 @@ import { Header as Container, Status, Text, Tooltip } from '@odigos/ui-kit/compo
 const resolvePercentDisplay = (percent: number): string => `${percent.toFixed(1)}%`
 
 export const Header = () => {
+  const { darkMode } = useDarkMode()
   const { epochInfo } = useEpochInfo()
   const { chainLoad, resolveChainLoadStatus } = useChainLoad()
 
@@ -58,7 +61,14 @@ export const Header = () => {
             />
           </Tooltip>,
         ]}
-        right={[<NewAirdrop key='new-airdrop' />, <ConnectWallet key='connect-wallet' />, <ToggleDarkMode key='toggle-theme' />]}
+        right={[
+          <SocialIcon key='social-twitter' network='twitter' url='https://x.com/intent/follow?screen_name=CardanoAirdrops' />,
+          <SocialIcon key='social-discord' url='https://discord.gg/H72xVv7fHd' />,
+          <SocialIcon key='social-github' url='https://github.com/BenElferink/ada-drop' />,
+          <NewAirdrop key='new-airdrop' />,
+          <ConnectWallet key='connect-wallet' />,
+          <ToggleDarkMode key='toggle-theme' />,
+        ]}
       />
     </MeshProvider>
   )
