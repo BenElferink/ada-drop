f3a9be5484d02e2f2c75cccec8da0534cfe0b9cd
Ben <ben.elferink@icloud.com>
2026-05-10 11:29:11 +0300

    chore: update deps (#25)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from 'next'
-import transpileModules from 'next-transpile-modules'
+import type { NextConfig } from "next";
+import transpileModules from "next-transpile-modules";
 
-const withTM = transpileModules(['@odigos/ui-kit', '@xyflow/react'])
+const withTM = transpileModules(["@odigos/ui-kit", "@xyflow/react"]);
 
 const nextConfig: NextConfig = withTM({
   reactStrictMode: true,
@@ -11,21 +11,45 @@ const nextConfig: NextConfig = withTM({
   compiler: {
     styledComponents: true,
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.experiments = {
       asyncWebAssembly: true,
       layers: true,
+    };
+
+    if (!isServer) {
+      const webpack = require("webpack");
+
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        buffer: require.resolve("buffer/"),
+        crypto: false,
+        process: require.resolve("process/browser"),
+      };
+
+      config.plugins.push(
+        new webpack.NormalModuleReplacementPlugin(
+          /^node:/,
+          (resource: { request: string }) => {
+            resource.request = resource.request.replace(/^node:/, "");
+          },
+        ),
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+          process: "process/browser",
+        }),
+      );
     }
 
     // Ensure CSS from transpiled node_modules works
     config.module.rules.push({
       test: /\.css$/,
       include: /node_modules[\\/](@odigos\/ui-kit|@xyflow\/react)/,
-      use: ['style-loader', 'css-loader'],
-    })
+      use: ["style-loader", "css-loader"],
+    });
 
-    return config
+    return config;
   },
-})
+});
 
-export default nextConfig
+export default nextConfig;
